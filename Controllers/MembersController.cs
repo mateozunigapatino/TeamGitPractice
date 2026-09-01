79883fc7744672e4d58c26cd4ba32336fc29013b
Carolina Pineda <carolinapinedarr@gmail.com>
2026-08-31 23:09:32 -0500

    Commit se agrega GET /api/members/count

--- Controllers/MembersController.cs
+++ Controllers/MembersController.cs
@@ -16,5 +16,9 @@ public class MembersController : ControllerBase
  };
         return Ok(members);
     }
-
+    [HttpGet("count")]
+    public IActionResult GetCount()
+    {
+        return Ok(new { total = 5 });
+    }
 }
