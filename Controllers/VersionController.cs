a5a9aa8e32070ce85f4138d91c9bc944454c074d
Mateo Zúñiga Patiño <ing.mateozunigapatino@gmail.com>
2026-09-02 19:13:35 -0500

    feat: extend version endpoint

--- Controllers/VersionController.cs
+++ Controllers/VersionController.cs
@@ -11,4 +11,11 @@ public class VersionController : ControllerBase
     {
         return Ok(new { version = "1.0.0" });
     }
+
+
+[HttpGet("platform")]
+    public IActionResult GetPlatform()
+    {
+        return Ok(new { platform = ".NET 10", project = "TeamGitPractice" });
+    }
 }
