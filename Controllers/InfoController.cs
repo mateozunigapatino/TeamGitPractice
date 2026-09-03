be9b0792e5a1f7768642fe99768cd83fb2d22d7f
Mateo Zúñiga Patiño <ing.mateozunigapatino@gmail.com>
2026-09-02 19:21:38 -0500

    feat: add info API endpoint

---
+++ Controllers/InfoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeamGitPractice.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class InfoController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetInfo()
+    {
+        return Ok(new { description = "Práctica colaborativa con Git" });
+    }
+}
